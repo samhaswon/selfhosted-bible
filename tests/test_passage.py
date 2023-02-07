0327852908ff6abf1eb0fc7c4976415aceaa054b
Samuel Howard <sahoward42@tntech.edu>
2023-02-07 14:25:54 -0600

    Added tests

--- tests/test_passage.py
+++ tests/test_passage.py
@@ -18,3 +18,8 @@ class TestPassage(TestCase):
                                                                                   "and the Word was God.",
                                                                                   "[1] In the beginning, God created "
                                                                                   "the heavens and the earth."])
+        self.assertEqual(self.passage.get_passage_esv("Psalm 117")[0], "[1] Praise the LORD, all nations!\n"
+                                                                       "        Extol him, all peoples!\n"
+                                                                       "    [2] For great is his steadfast love toward us,\n"
+                                                                       "        and the faithfulness of the LORD endures forever.\n"
+                                                                       "    Praise the LORD!")
