5ff537ce54d0cc4a86caab28d2fda948322aa440
Samuel Howard <sahoward42@tntech.edu>
2023-02-08 22:25:15 -0600

    Start of GUI

--- main.py
+++ main.py
@@ -1,8 +1,70 @@
 #!/usr/bin/env python
 
-from passage import Passage
+from esv import ESV
+from book import Book
+from navigate import Navigate, SelectChapter
+from flask import Flask, render_template, session, request, url_for, redirect
+from flask_bootstrap import Bootstrap
+from bokeh.resources import INLINE
 from typing import List
+import json
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'b^lC08A7d@z3'
+bootstrap = Bootstrap(app)
+
+books = ESV(False).books
+
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index.html', methods=['GET', 'POST'])
+def main_page():
+    # setup
+    js_resources = INLINE.render_js()
+    css_resources = INLINE.render_css()
+
+    script, div = None, None
+    debug = False
+
+    choices = [book.title for book in books]
+    type_sel = 'Select book'
+
+    if request.method == 'GET':
+        session.clear()
+    session['select_book'] = None
+    session['select_chapter'] = None
+    form = Navigate(choices=choices)
+
+    if form.validate_on_submit():
+        book = form.select_book.data
+        print(book)
+
+        chapter_count = 1
+        for index in books:
+            if index.title == book:
+                chapter_count = index.chapter_count
+                break
+
+        chapters: List[int] = [x for x in range(1, chapter_count + 1)]
+        form = Navigate(choices=chapters)
+
+    html = render_template('index.html', title='Home', formtitle='ESV Web', select_book=type_sel,
+                           books=choices, plot_script=script, debug=debug, form=form,
+                           plot_div=div, js_resources=js_resources, css_resources=css_resources)
+    return html
+
+
+@app.route('/chapter', methods=['GET', 'POST'])
+@app.route('/chapter.html', methods=['GET', 'POST'])
+def chapter():
+    # setup
+    js_resources = INLINE.render_js()
+    css_resources = INLINE.render_css()
+    book = session.get('book_selection')
+
+    html = render_template('chapter.html', title='Home')
+    return html
+
 
 if __name__ == '__main__':
-    passage = Passage(open("api-key.txt", "r").read())
-    print(passage.get_passage_esv("Genesis 1-3"))
+    app.run()
---
+++ navigate.py
@@ -0,0 +1,27 @@
+from wtforms import SubmitField, BooleanField, IntegerField, SelectField
+from wtforms.validators import DataRequired, NumberRange, Optional
+from flask_wtf import FlaskForm
+from typing import List, Tuple
+from esv import ESV
+
+
+class Navigate(FlaskForm):
+    books = ESV().books
+    choices: List[str] = [book.title for book in books]
+
+    select_chapter = SelectField('Select chapter', choices=[], validators=[Optional()])
+    select_book = SelectField('Select book', choices=choices, validators=[DataRequired()])
+    submit_book = SubmitField('Submit book')
+    submit_chapter = SubmitField('Submit chapter')
+
+    def __init__(self, choices: list, *args, **kwargs):
+        super(Navigate, self).__init__(*args, **kwargs)
+        self.select_chapter.choices = choices if type(choices[0]) is int else []
+
+
+class SelectChapter(FlaskForm):
+    books = ESV().books
+    choices: List[str] = [book.title for book in books]
+
+    select = SelectField('Chapter', choices=choices, validators=[DataRequired()])
+    submit = SubmitField('Display')
