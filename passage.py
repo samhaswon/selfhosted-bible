f303aa7c409546dfda8c1f091fdbb3a30f032f60
Samuel Howard <sahoward42@tntech.edu>
2023-02-08 18:23:16 -0600

    Added invalid passage exception

--- passage.py
+++ passage.py
@@ -10,18 +10,33 @@ class PassageNotFound(Exception):
     """
     Exception to be thrown whenever a query results in a passage not being found
     """
+
     def __init__(self, verse: str):
         super().__init__(str)
         self.__verse = verse
 
     def __str__(self):
-        return "PassageNotFound {}".format(self.__verse)
+        return "Passage not found {}".format(self.__verse)
+
+
+class PassageInvalid(Exception):
+    """
+    Exception to be thrown whenever a query results in a passage not being found
+    """
+
+    def __init__(self, verse: str):
+        super().__init__(str)
+        self.__verse = verse
+
+    def __str__(self):
+        return "Passage Invalid {}".format(self.__verse)
 
 
 class Passage:
     """
     Gets a passage of scripture from the ESV API
     """
+
     def __init__(self, key: str):
         """
         :param key: API key for requests
