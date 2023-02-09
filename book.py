98465fa5c00ea346cc088351aff50f6aa9d865bc
Samuel Howard <sahoward42@tntech.edu>
2023-02-08 18:19:13 -0600

    Added book definition

---
+++ book.py
@@ -0,0 +1,15 @@
+class Book:
+    """
+    Defines a book of the Bible. Used for safety constraints
+    """
+    def __init__(self, title: str, chapter_count: int):
+        self.__title = title
+        self.__chapter_count = chapter_count
+
+    @property
+    def title(self) -> str:
+        return self.__title
+
+    @property
+    def chapter_count(self) -> int:
+        return self.__chapter_count
