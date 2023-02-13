c4a20ccb1a1e685873467f65ec7b5621de48451f
Samuel Howard <sahoward42@tntech.edu>
2023-02-13 12:38:28 -0600

    Added relative navigation buttons

--- navigate.py
+++ navigate.py
@@ -5,11 +5,24 @@ from typing import List, Tuple
 from esv import ESV
 
 
+class NonValidatingSelectField(SelectField):
+    """
+    Class to ignore the SelectField validation
+    """
+    def pre_validate(self, form):
+        """
+        Does nothing
+        :param form: form to validate
+        :return: None
+        """
+        pass
+
+
 class Navigate(FlaskForm):
     books = ESV().books
     choices: List[str] = [book.title for book in books]
 
-    select_chapter = SelectField('Select chapter', choices=[], validators=[Optional()])
+    select_chapter = NonValidatingSelectField('Select chapter', choices=[], validators=[Optional()])
     select_book = SelectField('Select book', choices=choices, coerce=str, validators=[DataRequired()])
     submit_book = SubmitField('Submit book')
     submit_chapter = SubmitField('Submit chapter')
@@ -17,3 +30,8 @@ class Navigate(FlaskForm):
     def __init__(self, choices: list, *args, **kwargs):
         super(Navigate, self).__init__(*args, **kwargs)
         self.select_chapter.choices = choices if type(choices[0]) is int else []
+
+
+class NavigateRel(FlaskForm):
+    next_button = SubmitField("Next")
+    previous_button = SubmitField("Previous")
