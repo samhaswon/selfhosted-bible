7a8561c1d83b4e18d5c3e140efcfd65a5527ff61
Samuel Howard <sahoward42@tntech.edu>
2023-02-13 13:36:58 -0600

    Added passage naviagation to bible.py

--- bible.py
+++ bible.py
@@ -1,6 +1,8 @@
 from book import Book
 from typing import List, Tuple
 from abc import ABC, abstractmethod
+from multipledispatch import dispatch
+from typing import Tuple
 
 
 class Bible(ABC):
@@ -9,73 +11,74 @@ class Bible(ABC):
     """
 
     def __init__(self):
-        self.__books_of_the_bible: List[Tuple[str, int]] = [("Genesis", 50),
-                                                            ("Exodus", 40),
-                                                            ("Leviticus", 27),
-                                                            ("Numbers", 36),
-                                                            ("Deuteronomy", 34),
-                                                            ("Joshua", 24),
-                                                            ("Judges", 21),
-                                                            ("Ruth", 4),
-                                                            ("1 Samuel", 31),
-                                                            ("2 Samuel", 24),
-                                                            ("1 Kings", 22),
-                                                            ("2 Kings", 25),
-                                                            ("1 Chronicles", 29),
-                                                            ("2 Chronicles", 36),
-                                                            ("Ezra", 10),
-                                                            ("Nehemiah", 13),
-                                                            ("Esther", 10),
-                                                            ("Job", 42),
-                                                            ("Psalm", 150),
-                                                            ("Proverbs", 31),
-                                                            ("Ecclesiastes", 12),
-                                                            ("Song of Solomon", 8),
-                                                            ("Isaiah", 66),
-                                                            ("Jeremiah", 52),
-                                                            ("Lamentations", 5),
-                                                            ("Ezekiel", 48),
-                                                            ("Daniel", 12),
-                                                            ("Hosea", 14),
-                                                            ("Joel", 3),
-                                                            ("Amos", 9),
-                                                            ("Obadiah", 1),
-                                                            ("Jonah", 4),
-                                                            ("Micah", 7),
-                                                            ("Nahum", 3),
-                                                            ("Habakkuk", 3),
-                                                            ("Zephaniah", 3),
-                                                            ("Haggai", 2),
-                                                            ("Zechariah", 14),
-                                                            ("Malachi", 4),
-                                                            ("Matthew", 28),
-                                                            ("Mark", 16),
-                                                            ("Luke", 24),
-                                                            ("John", 21),
-                                                            ("Acts", 28),
-                                                            ("Romans", 16),
-                                                            ("1 Corinthians", 16),
-                                                            ("2 Corinthians", 13),
-                                                            ("Galatians", 6),
-                                                            ("Ephesians", 6),
-                                                            ("Philippians", 4),
-                                                            ("Colossians", 4),
-                                                            ("1 Thessalonians", 5),
-                                                            ("2 Thessalonians", 3),
-                                                            ("1 Timothy", 6),
-                                                            ("2 Timothy", 4),
-                                                            ("Titus", 3),
-                                                            ("Philemon", 1),
-                                                            ("Hebrews", 13),
-                                                            ("James", 5),
-                                                            ("1 Peter", 5),
-                                                            ("2 Peter", 3),
-                                                            ("1 John", 5),
-                                                            ("2 John", 1),
-                                                            ("3 John", 1),
-                                                            ("Jude", 1),
-                                                            ("Revelation", 22)]
-        self.__books: List[Book] = [Book(title, chapters) for title, chapters in self.__books_of_the_bible]
+        books_of_the_bible: List[Tuple[str, int]] = [("Genesis", 50),
+                                                     ("Exodus", 40),
+                                                     ("Leviticus", 27),
+                                                     ("Numbers", 36),
+                                                     ("Deuteronomy", 34),
+                                                     ("Joshua", 24),
+                                                     ("Judges", 21),
+                                                     ("Ruth", 4),
+                                                     ("1 Samuel", 31),
+                                                     ("2 Samuel", 24),
+                                                     ("1 Kings", 22),
+                                                     ("2 Kings", 25),
+                                                     ("1 Chronicles", 29),
+                                                     ("2 Chronicles", 36),
+                                                     ("Ezra", 10),
+                                                     ("Nehemiah", 13),
+                                                     ("Esther", 10),
+                                                     ("Job", 42),
+                                                     ("Psalm", 150),
+                                                     ("Proverbs", 31),
+                                                     ("Ecclesiastes", 12),
+                                                     ("Song of Solomon", 8),
+                                                     ("Isaiah", 66),
+                                                     ("Jeremiah", 52),
+                                                     ("Lamentations", 5),
+                                                     ("Ezekiel", 48),
+                                                     ("Daniel", 12),
+                                                     ("Hosea", 14),
+                                                     ("Joel", 3),
+                                                     ("Amos", 9),
+                                                     ("Obadiah", 1),
+                                                     ("Jonah", 4),
+                                                     ("Micah", 7),
+                                                     ("Nahum", 3),
+                                                     ("Habakkuk", 3),
+                                                     ("Zephaniah", 3),
+                                                     ("Haggai", 2),
+                                                     ("Zechariah", 14),
+                                                     ("Malachi", 4),
+                                                     ("Matthew", 28),
+                                                     ("Mark", 16),
+                                                     ("Luke", 24),
+                                                     ("John", 21),
+                                                     ("Acts", 28),
+                                                     ("Romans", 16),
+                                                     ("1 Corinthians", 16),
+                                                     ("2 Corinthians", 13),
+                                                     ("Galatians", 6),
+                                                     ("Ephesians", 6),
+                                                     ("Philippians", 4),
+                                                     ("Colossians", 4),
+                                                     ("1 Thessalonians", 5),
+                                                     ("2 Thessalonians", 3),
+                                                     ("1 Timothy", 6),
+                                                     ("2 Timothy", 4),
+                                                     ("Titus", 3),
+                                                     ("Philemon", 1),
+                                                     ("Hebrews", 13),
+                                                     ("James", 5),
+                                                     ("1 Peter", 5),
+                                                     ("2 Peter", 3),
+                                                     ("1 John", 5),
+                                                     ("2 John", 1),
+                                                     ("3 John", 1),
+                                                     ("Jude", 1),
+                                                     ("Revelation", 22)]
+        self.__books: List[Book] = [Book(title, chapters) for title, chapters in books_of_the_bible]
+        self.__books_of_the_bible: dict = dict(books_of_the_bible)
 
     @property
     def books(self):
@@ -84,3 +87,34 @@ class Bible(ABC):
     @abstractmethod
     def get_passage(self, book, chapter):
         raise NotImplementedError
+
+    @dispatch(str, str)
+    def next_passage(self, book: str, chapter: str) -> Tuple[str, str]:
+        return self.next_passage(book, int(chapter))
+
+    @dispatch(str, int)
+    def next_passage(self, book: str, chapter: int) -> Tuple[str, str]:
+        it_books = iter(self.__books_of_the_bible)
+        for key in it_books:
+            if key == book:
+                if chapter == self.__books_of_the_bible[key]:
+                    return next(it_books, "Genesis"), "1"
+                else:
+                    return book, str(chapter + 1)
+
+    @dispatch(str, str)
+    def previous_passage(self, book: str, chapter: str) -> Tuple[str, str]:
+        return self.previous_passage(book, int(chapter))
+
+    @dispatch(str, int)
+    def previous_passage(self, book: str, chapter: int) -> Tuple[str, str]:
+        it_books = iter(self.__books_of_the_bible)
+        previous_key = "Revelation"
+        for key in it_books:
+            if key == book:
+                if chapter == 1:
+                    return previous_key, str(self.__books_of_the_bible[previous_key])
+                else:
+                    return book, str(chapter - 1)
+            else:
+                previous_key = key
---
+++ tests/test_bible.py
@@ -0,0 +1,25 @@
+from unittest import TestCase
+from bible import Bible
+
+
+class BibleTest(Bible):
+    def __init__(self):
+        super().__init__()
+
+    def get_passage(self, book, chapter):
+        pass
+
+
+class TestBible(TestCase):
+    def setUp(self) -> None:
+        self.bible = BibleTest()
+
+    def test_previous_passage(self):
+        self.assertEqual(self.bible.previous_passage("John", 3), ("John", "2"))
+        self.assertEqual(self.bible.previous_passage("John", 1), ("Luke", "24"))
+        self.assertEqual(self.bible.previous_passage("Genesis", 1), ("Revelation", "22"))
+
+    def test_next_passage(self):
+        self.assertEqual(self.bible.next_passage("John", 4), ("John", "5"))
+        self.assertEqual(self.bible.next_passage("Luke", 24), ("John", "1"))
+        self.assertEqual(self.bible.next_passage("Revelation", 22), ("Genesis", "1"))
