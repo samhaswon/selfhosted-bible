465fc99f145cd9a234f1bfd2b1f32a8c6f75311d
Samuel Howard <sahoward42@tntech.edu>
2023-03-13 14:38:25 -0500

    Finished rebranding

--- setup.py
+++ setup.py
@@ -5,19 +5,18 @@ here = pathlib.Path(__file__).parent.resolve()
 long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
-    name='esv-web',
+    name='self-hosted-bible',
     version='1.0.0',
-    description="A self-hosted webapp connecting to Crossway's ESV API",
+    description="A self-hosted webapp of various Bible versions.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/samhaswon/ESV-web",
+    url="https://github.com/samhaswon/selfhosted-bible",
     author="Samuel Howard",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'flask',
         'requests',
-        'pymongo',
         'multipledispatch',
         'requests-cache',
         'flask-bootstrap',
