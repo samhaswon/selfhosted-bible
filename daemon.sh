bcb4493884cbe31150b603672939b0a4c7c71f9d
Samuel Howard <sahoward42@tntech.edu>
2023-03-13 14:53:05 -0500

    Updated daemon to reflect overall changes

--- daemon.sh
+++ daemon.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ -n "$API_KEY" ]; then
-  echo -n "$API_KEY" > '/usr/src/app/api-key.txt'
+  echo -n "$API_KEY" > '/usr/src/app/esv-api-key.txt'
 fi
 
 exec waitress-serve --port=5000 --call "main:create_app"
