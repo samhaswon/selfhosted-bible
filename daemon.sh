d986aef8be6176fa4d5fdd1dde6d92d0143a56cf
Samuel Howard <sahoward42@tntech.edu>
2023-03-14 17:28:10 -0500

    Added unauthed option to daemon

--- daemon.sh
+++ daemon.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-if [ -n "$API_KEY" ]; then
+if [ -n "$API_KEY" ]
+then
   echo -n "$API_KEY" > '/usr/src/app/esv-api-key.txt'
+else
+  echo -n "unauthed" > '/usr/src/app/esv-api-key.txt'
 fi
 
 exec waitress-serve --port=5000 --call "main:create_app"
