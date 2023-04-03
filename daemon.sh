1d08cb8e6ae234792033b11ae334b5ca600c150e
Samuel Howard <sahoward42@tntech.edu>
2023-04-03 11:50:36 -0500

    Made daemon more concise

--- daemon.sh
+++ daemon.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env ash
 
-if [ -n "$API_KEY" ]
-then
-  echo -n "$API_KEY" > '/usr/src/app/esv-api-key.txt'
-else
-  echo -n "" > '/usr/src/app/esv-api-key.txt'
-fi
+[[ -z "${ESV_API_KEY}" || -z "${DOCKER_STARTED}" ]] && echo -n "$ESV_API_KEY" > '/usr/src/app/esv-api-key.txt' || echo -n "" > '/usr/src/app/esv-api-key.txt'
+export DOCKER_STARTED="true"
 
 set -e
-
 exec waitress-serve --port=5000 --call "main:create_app"
