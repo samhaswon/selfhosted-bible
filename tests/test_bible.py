a8c93790f019fe9da1397cacd04f33d94f9a1794
Samuel Howard <sahoward42@tntech.edu>
2023-04-05 11:56:57 -0500

    Slightly modified tests

--- tests/test_bible.py
+++ tests/test_bible.py
@@ -16,12 +16,12 @@ class TestBible(TestCase):
 
     def test_previous_passage(self):
         self.assertEqual(self.bible.previous_passage("John", 3), ("John", "2"))
-        self.assertEqual(self.bible.previous_passage("John", 1), ("Luke", "24"))
+        self.assertEqual(self.bible.previous_passage("John", "1"), ("Luke", "24"))
         self.assertEqual(self.bible.previous_passage("Genesis", 1), ("Revelation", "22"))
 
     def test_next_passage(self):
         self.assertEqual(self.bible.next_passage("John", 4), ("John", "5"))
-        self.assertEqual(self.bible.next_passage("Luke", 24), ("John", "1"))
+        self.assertEqual(self.bible.next_passage("Luke", "24"), ("John", "1"))
         self.assertEqual(self.bible.next_passage("Revelation", 22), ("Genesis", "1"))
 
     def test_has_passage(self):
