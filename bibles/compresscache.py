edd4b6b0c23662fa8af981ccbb17153d17a4db7e
Samuel Howard <sahoward42@tntech.edu>
2023-07-19 23:38:30 -0400

    Fixed a path error PyCharm didn't change
    Signed-off-by: Samuel Howard <sahoward42@tntech.edu>

--- bibles/compresscache.py
+++ bibles/compresscache.py
@@ -21,11 +21,11 @@ class CompressCache(object):
         """
         # Normal save
         try:
-            with bz2.open(f"bibles/json_bibles/{self.__name}.json.pbz2", "wb") as data_file:
+            with bz2.open(f"bibles/json-bibles/{self.__name}.json.pbz2", "wb") as data_file:
                 data_file.write(json.dumps(data).encode('utf-8'))
         # Testing save
         except FileNotFoundError:
-            with bz2.open(f"../bibles/json_bibles/{self.__name}.json.pbz2", "wb") as data_file:
+            with bz2.open(f"../bibles/json-bibles/{self.__name}.json.pbz2", "wb") as data_file:
                 data_file.write(json.dumps(data).encode('utf-8'))
 
     def load(self) -> dict:
@@ -33,16 +33,5 @@ class CompressCache(object):
         Loads the compressed JSON of the Bible
         :return: dictionary version of the loaded JSON
         """
-        with bz2.open(f"bibles/json_bibles/{self.__name}.json.pbz2", "rt", encoding='utf-8') as data_file:
+        with bz2.open(f"bibles/json-bibles/{self.__name}.json.pbz2", "rt", encoding='utf-8') as data_file:
             return json.load(data_file)
-
-if __name__ == '__main__':
-    ver = "ylt"
-    with open(f"../bibles/json_bibles/{ver}.json", "r") as data_file:
-        file = json.load(data_file)
-
-    with bz2.open(f"../bibles/json_bibles/{ver}.json.pbz2", "wb") as data_file:
-        data_file.write(json.dumps(file).encode('utf-8'))
-
-    with bz2.open(f"../bibles/json_bibles/{ver}.json.pbz2", "rt", encoding='utf-8') as data_file:
-        print(json.load(data_file))
