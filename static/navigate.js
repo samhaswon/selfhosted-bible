a668277f6f1cf445c9d3d1116f545a667ba59def
Samuel Howard <sahoward42@tntech.edu>
2023-08-03 09:39:04 -0400

    Dynamic form updating code
    Signed-off-by: Samuel Howard <sahoward42@tntech.edu>

---
+++ static/navigate.js
@@ -0,0 +1,28 @@
+const bookSelect = document.querySelector('#book');
+const chapterSelect = document.querySelector('#chapter');
+
+function updateChapterSelect() {
+  // get the selected book
+  const book = bookSelect.value;
+  // make an AJAX request to the /chapters route to get the number of chapters for the selected book
+  fetch(`/chapters/${book}`)
+    .then(response => response.json())
+    .then(data => {
+      // remove all options from the chapter select
+      chapterSelect.innerHTML = '';
+      // get the number of chapters from the JSON response
+      const numChapters = data.num_chapters;
+      // add options for each chapter
+      for (let i = 1; i <= numChapters; i++) {
+        const option = document.createElement('option');
+        option.value = i;
+        option.textContent = i;
+        chapterSelect.appendChild(option);
+      }
+    });
+}
+
+bookSelect.addEventListener('change', updateChapterSelect);
+
+// trigger the change event on the book select when the page loads
+updateChapterSelect();
