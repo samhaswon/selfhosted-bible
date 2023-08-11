aa851af2fc9dfabeeedb7d52e008a9bac6df4efa
Samuel Howard <sahoward42@tntech.edu>
2023-08-11 18:33:39 -0500

    Minified javascript, keeping a readable version
    Signed-off-by: Samuel Howard <sahoward42@tntech.edu>

---
+++ static/navigate_readable.js
@@ -0,0 +1,32 @@
+// Minify using: https://www.digitalocean.com/community/tools/minify
+const bookSelect = document.querySelector('#book');
+const chapterSelect = document.querySelector('#chapter');
+
+function updateChapterSelect() {
+  // get the selected book
+  const book = bookSelect.value;
+  // make an AJAX request to the /chapters route to get the number of chapters for the selected book
+  fetch(`/chapters/${book}`)
+    .then(response => response.json())
+    .then(data => {
+      // remove all options from the chapter select
+      chapterSelect.innerHTML = '';
+      // get the number of chapters from the JSON response
+      const numChapters = data.num_chapters;
+      // add options for each chapter
+      for (let i = 1; i <= numChapters; i++) {
+        const option = document.createElement('option');
+        option.value = i;
+        option.textContent = i;
+        if (window.chapterNumber && window.chapterNumber === i) {
+            option.selected = true;
+        }
+        chapterSelect.appendChild(option);
+      }
+    });
+}
+
+bookSelect.addEventListener('change', updateChapterSelect);
+
+// trigger the change event on the book select when the page loads
+updateChapterSelect();
