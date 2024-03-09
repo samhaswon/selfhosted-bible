1aab0ac567e5a294fa8b5c1df97fa33313aa7562
Samuel Howard <sahoward42@tntech.edu>
2024-03-08 19:38:31 -0600

    XSS mitigation
    Signed-off-by: Samuel Howard <sahoward42@tntech.edu>

--- static/search_readable.js
+++ static/search_readable.js
@@ -20,10 +20,13 @@ function updateResults(results) {
     searchResults.innerHTML = '';
 
     for (let i = 0; i < results.length; i++) {
-        const book = results[i][0].substring(0, results[i][0].indexOf(" "));
-        const chapter = results[i][0].substring(results[i][0].indexOf(" ") + 1, results[i][0].indexOf(":"));
+        const book = results[i][0].substring(0, results[i][0].lastIndexOf(" "));
+        const chapter = results[i][0].substring(results[i][0].lastIndexOf(" ") + 1, results[i][0].indexOf(":"));
 
-        const ref = `/goto/?version=${versionSelection.value}&book=${book}&chapter=${chapter}`;
+        const version = encodeURIComponent(versionSelection.value);
+        const bookEncoded = encodeURIComponent(book);
+        const chapterEncoded = encodeURIComponent(chapter);
+        const ref = `/goto/?version=${version}&book=${bookEncoded}&chapter=${chapterEncoded}`;
         const ref_child = document.createElement('a');
         ref_child.href = ref;
         ref_child.textContent = results[i][0];
@@ -57,16 +60,4 @@ versionSelection.addEventListener('change', (event) => {
     }).catch((error) => {
         console.log(error);
     });
-});
-
-// Add event listener to the list
-searchResults.addEventListener('click', function(event) {
-    // Check if the clicked element is a list item
-    if (event.target.tagName === 'LI') {
-        // Get the text content of the clicked list item
-        const reference = event.target.textContent.trim();
-
-        // Set the Flask cookie with the reference
-        document.cookie = `reference=${reference}; path=/`; // Adjust the path as needed
-    }
 });
