c42064f9fd09defc74061ca176dd4ecce4db132f
Samuel Howard <sahoward42@tntech.edu>
2024-03-08 18:32:30 -0600

    Updated path handling
    Signed-off-by: Samuel Howard <sahoward42@tntech.edu>

--- bibles/compresscache.py
+++ bibles/compresscache.py
@@ -1,5 +1,6 @@
-import json
 import bz2
+import json
+import os
 
 
 class CompressCache(object):
@@ -12,32 +13,21 @@ class CompressCache(object):
         :returns: None
         """
         self.__name = name
+        self.__base_path = os.path.dirname(os.path.abspath(__file__))
 
     def save(self, data: dict) -> None:
         """
-        Saves the given data with the given version name
-        :param data:
-        :return:
+        Saves the given data with the given version name.
+        :param data: the Dictionary data to save.
+        :return: None
         """
-        # Normal save
-        try:
-            with bz2.open(f"bibles/json-bibles/{self.__name}.json.pbz2", "wb") as data_file:
-                data_file.write(json.dumps(data, separators=(',', ':')).encode('utf-8'))
-        # Testing save
-        except FileNotFoundError:
-            with bz2.open(f"../bibles/json-bibles/{self.__name}.json.pbz2", "wb") as data_file:
-                data_file.write(json.dumps(data, separators=(',', ':')).encode('utf-8'))
+        with bz2.open(f"{self.__base_path}/json-bibles/{self.__name}.json.pbz2", "wb") as data_file:
+            data_file.write(json.dumps(data, separators=(',', ':')).encode('utf-8'))
 
     def load(self) -> dict:
         """
-        Loads the compressed JSON of the Bible
-        :return: dictionary version of the loaded JSON
+        Loads the compressed JSON of the Bible.
+        :return: The dictionary version of the loaded JSON
         """
-        # Normal load
-        try:
-            with bz2.open(f"bibles/json-bibles/{self.__name}.json.pbz2", "rt", encoding='utf-8') as data_file:
-                return json.load(data_file)
-        # Testing load
-        except FileNotFoundError:
-            with bz2.open(f"../bibles/json-bibles/{self.__name}.json.pbz2", "rt", encoding='utf-8') as data_file:
-                return json.load(data_file)
+        with bz2.open(f"{self.__base_path}/json-bibles/{self.__name}.json.pbz2", "rt", encoding='utf-8') as data_file:
+            return json.load(data_file)
