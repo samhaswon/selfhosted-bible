3d119969343a6dbc38df3e72ba15cd2d007c8198
Samuel Howard <sahoward42@tntech.edu>
2024-03-09 11:19:09 -0600

    Added bible4u.net translation
    Signed-off-by: Samuel Howard <sahoward42@tntech.edu>

---
+++ scripts/xml_to_json.py
@@ -0,0 +1,33 @@
+import json
+import xml.etree.ElementTree as ET
+from bibles.compresscache import CompressCache
+
+
+if __name__ == '__main__':
+    json_version = {}
+
+    tree = ET.parse("ACV.xml")
+    root = tree.getroot()
+    for book in root:
+        if book.tag == 'INFORMATION':
+            continue
+        name = book.attrib["bname"]
+        if name.startswith("I "):
+            name = "1" + name[1:]
+        elif name.startswith("II "):
+            name = "2" + name[2:]
+
+        json_version[name] = {}
+
+        for chapter in book:
+            number = chapter.attrib["cnumber"]
+            json_version[name][number] = []
+            for verse in chapter:
+                content = verse.attrib["vnumber"] + " " + verse.text
+                json_version[name][number].append(content)
+
+    with open("acv.json", "w") as json_file:
+        json_file.write(json.dumps(json_version))
+
+    compress_cache = CompressCache("acv")
+    compress_cache.save(json_version)
