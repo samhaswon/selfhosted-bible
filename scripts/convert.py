14b0ad3395bb67f92431f3a079d0f96a1d2df0ac
Samuel Howard <sahoward42@tntech.edu>
2024-11-30 10:21:44 -0600

    Add compression to conversion script.
    Signed-off-by: Samuel Howard <sahoward42@tntech.edu>

--- scripts/convert.py
+++ scripts/convert.py
@@ -4,9 +4,11 @@ Converts JSON from bolls.life (https://bolls.life/api/)
 import json
 import re
 from bibles.kjv import KJV
+from bibles.compresscache import CompressCache
 
 if __name__ == '__main__':
-    filename = "ylt.json"
+    filename = "../BTX3.json"
+    abbreviation = "btx3"
     tag_remover = re.compile(r'<.*?>')
     kjv_obj = KJV()
     bible = {book.name: {str(chapter): [] for chapter in range(1, book.chapter_count + 1)} for book in KJV().books}
@@ -23,3 +25,5 @@ if __name__ == '__main__':
         bible[book_name][str(chapter)].append(f"{verse_num} {content}")
     with open(filename, "w") as bible_save:
         json.dump(bible, bible_save)
+    compress_cache = CompressCache(abbreviation)
+    compress_cache.save(bible)
